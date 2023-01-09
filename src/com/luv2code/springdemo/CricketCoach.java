fd126a20adedf0aa6cf3137c99ca679645dd5c45
rizki eka <rizky10eka@gmail.com>
2023-01-09 11:41:29 +0700

    inject setter value on pom

---
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -0,0 +1,59 @@
+package com.luv2code.springdemo;
+
+public class CricketCoach implements Coach {
+
+	private FortuneService fortuneService;
+	
+	// add new field for emailAddress and team
+	private String emailAddress;
+	private String team;
+	
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach : inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach : inside setter method - setTeam");
+		this.team = team;
+	}
+
+	// create a no-arg constructor 
+	public CricketCoach() {
+		System.out.println("CricketCoach : inside no-arg constructor");
+	}
+	
+	// our setter method
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("CricketCoach : inside setter method - fortuneService");
+		this.fortuneService = fortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Practice fast bowling for 15 minutes";
+	}
+
+	@Override
+	public String getSetupPlayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getFortune();
+	}
+
+}
